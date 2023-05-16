5b4540dda3cbc6482e29a3c40c04e723cd5f5849
tp-wsb <133579874+tp-wsb@users.noreply.github.com>
2023-05-16 18:07:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-     return '<h1>Hello WSB! Greetings from Flask!</h1>'
+    """Handler for the root URL."""
+    return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == "__main__":
-     app.run(debug=True)
+    app.run(debug=True)
