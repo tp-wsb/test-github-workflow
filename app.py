480a7a7c3983031d1fc17d61bbf9ffb6af6d49a0
tp-wsb <133579874+tp-wsb@users.noreply.github.com>
2023-05-16 16:50:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
-
 @app.route('/')
 def index():
-<<<<<<< HEAD
-    return '<h1>Hello WSB! Greetings from Flask!</h1>'
-
+ return '<h1>Hello WSB! Greetings from Flask!</h1>'
 if __name__ == "__main__":
-    app.run(debug=True)
-=======
-	return '<h1>Hello WSB! Greetings from Flask!</h1>'
-if __name__ == "__main__":
-	app.run(debug=True)
->>>>>>> ebfa888 (requirements.txt)
+ app.run(debug=True)
