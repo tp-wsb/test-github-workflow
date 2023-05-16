c5557fa1f6372697236127abe7019685aacde6c3
tp-wsb <133579874+tp-wsb@users.noreply.github.com>
2023-05-16 18:09:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+"""
+This is a Flask application that greets WSB.
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """Handler for the root URL."""
+    """
+    Handler for the root URL.
+    """
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == "__main__":
