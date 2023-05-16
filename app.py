7f83fe2c24105568ad67953f815491c1d021d970
root <root@GL11MCDS.bakers-ent.global>
2023-05-16 14:51:26 +0200

    requirements.txt

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+<<<<<<< HEAD
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == "__main__":
     app.run(debug=True)
+=======
+	return '<h1>Hello WSB! Greetings from Flask!</h1>'
+if __name__ == "__main__":
+	app.run(debug=True)
+>>>>>>> ebfa888 (requirements.txt)
